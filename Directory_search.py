4c4912b4d4ae7fed7ab99da84e04d5df216dc93b
Qrowe <42359258+Qrowe@users.noreply.github.com>
2018-12-03 17:16:25 +0300

    Homework.

--- Directory_search.py
+++ Directory_search.py
@@ -4,9 +4,9 @@ import os  # Для работы с директориями в операцио
 
 
 def main():
-    input_func = input
 
-    CheckDir = input_func('Введите расположение и имя директории')
+
+    CheckDir = input('Введите расположение и имя директории')
     print()
 
     if os.path.exists(CheckDir):  # Проверяем, существует ли эта директория в нашей системе
